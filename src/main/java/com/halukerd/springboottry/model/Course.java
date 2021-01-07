f3a1fed7522ba61f020fc9f77a62b7c0427a33b5
HalukErd <erd.haluk@gmail.com>
2021-01-07 16:43:02 +0300

    Student and Courses were joined each other with enrolment table without Enrolment.java entity

---
+++ src/main/java/com/halukerd/springboottry/model/Course.java
@@ -0,0 +1,62 @@
+package com.halukerd.springboottry.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+
+@Entity(name = "Course")
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_sequence_generator"
+    )
+    @SequenceGenerator(
+            name = "course_sequence_generator",
+            sequenceName = "course_sequence_generator",
+            allocationSize = 1
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NonNull
+    private String name;
+
+    @Column(
+            name = "department",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NonNull
+    private String department;
+
+    @ManyToMany(mappedBy = "courses")
+    private List<Student> students = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", students=" + students +
+                '}';
+    }
+}
---
+++ src/main/java/com/halukerd/springboottry/model/Enrolment.java
@@ -0,0 +1,29 @@
+package com.halukerd.springboottry.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+//@Entity(name = "Enrolment")
+//@Table(name = "enrolment")
+public class Enrolment {
+
+//    @Id
+//    @GeneratedValue(
+//            strategy = GenerationType.SEQUENCE,
+//            generator = "enrolment_sequence_generator"
+//    )
+//    @SequenceGenerator(
+//            name = "enrolment_sequence_generator",
+//            sequenceName = "enrolment_sequence_generator",
+//            allocationSize = 1
+//    )
+//    @Column(
+//            name = "id"
+//    )
+//    private Long id;
+//
+//    @ManyToMany
+//    private Student student;
+//    private Course course;
+//    private LocalDateTime createdAt;
+}
